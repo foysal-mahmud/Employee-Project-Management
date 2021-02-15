a783bf8a246ad53b6653ef313acb6f3259c87c81
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2021-02-15 22:58:37 +0600

    Delete src/main directory

--- src/main/java/com/foysalSpring/pma/dao/UserAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foysalSpring.pma.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.foysalSpring.pma.entities.UserAccount;
-
-public interface UserAccountRepository extends CrudRepository<UserAccount, Long>{
-
-}
